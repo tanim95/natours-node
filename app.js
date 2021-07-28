759848aa2e7c713c68c2529543a080518b6ac348
tanim95 <tanimrahman78@gmail.com>
2021-07-28 22:23:53 +0600

    adde aggregation pipeline unwindig method

--- app.js
+++ app.js
@@ -72,6 +72,58 @@ const aggregationPipeline = async (req, res) => {
     });
   }
 };
+
+const getMonthlyPlan = async (req, res) => {
+  try {
+    const year = +req.params.year;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTours: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: { _id: 0 },
+      },
+      {
+        $sort: {
+          numTours: -1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Failed',
+      message: err,
+    });
+  }
+};
+
+app.get('/api/v1/tours/monthly-plan/:year', getMonthlyPlan);
+
 app.get('/api/v1/tours/tour-stats', aggregationPipeline);
 
 const getAllTours = async (req, res) => {
